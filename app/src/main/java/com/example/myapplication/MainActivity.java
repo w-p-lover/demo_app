533c80323d8e8d5029e2dd68084ae2777c0e6410
wangyoupeng2 <109458757+wangyoupeng2@users.noreply.github.com>
2024-05-24 17:27:01 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -23,12 +23,64 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void buttonread__Click(View view) {
-    }
 
     public void buttonsave_Click(View view) {
+        getUserName = (EditText) findViewById(R.id.editUserName);
+        getUserPassword = (EditText) findViewById(R.id.editPassword);
+        getUserTel = (EditText) findViewById(R.id.editTel);
+        getUserEmail = (EditText) findViewById(R.id.editEmail);
+        getUserSex = (Spinner) findViewById(R.id.choosesex);
+        
+        SharedPreferences pref = getSharedPreferences("user_info",MODE_PRIVATE);
+        SharedPreferences.Editor editor =pref.edit();
+        editor.putString("username",getUserName.getText().toString());
+        editor.putString("userpassword",getUserPassword.getText().toString());
+        editor.putString("usertel",getUserTel.getText().toString());
+        editor.putString("useremail",getUserEmail.getText().toString());
+        editor.putString("usersex",getUserSex.getSelectedItem().toString());
+        editor.apply();
+        Toast.makeText(MainActivity.this,"保存成功",Toast.LENGTH_LONG).show();
     }
-
-    public void buttonclean__Click(View view) {
+ 
+    public void buttonread__Click(View view)
+    {
+        getUserName = (EditText) findViewById(R.id.editUserName);
+        getUserPassword = (EditText) findViewById(R.id.editPassword);
+        getUserTel = (EditText) findViewById(R.id.editTel);
+        getUserEmail = (EditText) findViewById(R.id.editEmail);
+        getUserSex = (Spinner) findViewById(R.id.choosesex);
+ 
+        SharedPreferences pref = getSharedPreferences("user_info",MODE_PRIVATE);
+        getUserName.setText(pref.getString("username",""));
+        getUserPassword.setText(pref.getString("userpassword",""));
+        getUserTel.setText(pref.getString("usertel",""));
+        getUserEmail.setText(pref.getString("useremail",""));
+        SpinnerAdapter spAdapter = getUserSex.getAdapter();
+        int k = spAdapter.getCount();
+        for (int i=0;i<k;i++)
+        {
+            if(pref.getString("usersex","").equals(spAdapter.getItem(i)))
+            {
+                getUserSex.setSelection(i);
+                break;
+            }
+        }
+        Toast.makeText(MainActivity.this,"读取成功",Toast.LENGTH_LONG).show();
+    }
+ 
+    public void buttonclean__Click(View view)
+    {
+        getUserName = (EditText) findViewById(R.id.editUserName);
+        getUserPassword = (EditText) findViewById(R.id.editPassword);
+        getUserTel = (EditText) findViewById(R.id.editTel);
+        getUserEmail = (EditText) findViewById(R.id.editEmail);
+        getUserSex = (Spinner) findViewById(R.id.choosesex);
+ 
+        getUserSex.setSelection(0);
+        getUserName.setText("");
+        getUserPassword.setText("");
+        getUserEmail.setText("");
+        getUserTel.setText("");
+        Toast.makeText(MainActivity.this,"清空成功",Toast.LENGTH_LONG).show();
     }
 }
